b1325fabdfcb68fdc9bb729ad0c5369d89476efd
ChangleCat <513618438@qq.com>
2025-07-16 14:31:38 +0800

    day 15 尝试使用web component

---
+++ 15-Increment Counter/script.js
@@ -0,0 +1,46 @@
+class CounterContainer extends HTMLElement {
+
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: 'closed' });
+
+    this.target = parseInt(this.getAttribute('target'));
+    this.nowNum = 0;
+    this.DURATION = 1000;
+    let templateElem = document.getElementById('increment-counter-template');
+    let content = templateElem.content.cloneNode(true);
+    content.querySelector('.icon').innerText = this.getAttribute('icon');
+    content.querySelector('.counter').innerText = 0;
+    content.querySelector('.counter').setAttribute("data-target", this.target);
+    content.querySelector('.explain').innerText = this.getAttribute("explain");
+    this.shadow.appendChild(content);
+
+    // this.content = content;
+    this.startTime = null;
+
+    //! 非常重要，这个函数的this必须指向这个CounterContainer的实例
+    this.requestIncrementAnimation = this.requestIncrementAnimation.bind(this);
+  }
+
+  connectedCallback() {
+    requestAnimationFrame(this.requestIncrementAnimation);
+  }
+
+  requestIncrementAnimation(timestamp) {
+    if (!this.startTime) {
+      this.startTime = timestamp;
+    }
+    const deltaTime = timestamp - this.startTime
+
+    const nowNum = Math.floor(this.target * deltaTime / this.DURATION);
+
+    if (deltaTime < this.DURATION) {
+      this.shadow.querySelector('.counter').innerText = nowNum.toString();
+      requestAnimationFrame(this.requestIncrementAnimation);
+    } else {
+      this.shadow.querySelector('.counter').innerText = this.target;
+    }
+  }
+}
+
+customElements.define("counter-container", CounterContainer);
